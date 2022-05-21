08fe244829aaa6bcdba1f9b0957f4d808246aacb
Mike Kale <kaledev@gmail.com>
2022-05-20 21:02:47 -0400

    Update on-air-display.ino
    Customization changes

--- on-air-display/on-air-display.ino
+++ on-air-display/on-air-display.ino
@@ -13,41 +13,57 @@ MQTTClient client;
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 int led_color = strip.Color(0, 150, 0);
 
-
 void initWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
+
+    strip.clear();
+    led_color = strip.Color(0, 160, 255); // Blue
+    for (int i = 0; i < LED_COUNT; i++) {
+      strip.setPixelColor(i, led_color);
+      strip.show();
+    }
+    
     delay(500);
   }
+  
+  strip.clear();
+  strip.show();
+  
   Serial.print("\nIp: ");
   Serial.println(WiFi.localIP());
-
 }
 
 void initIOT()
 {
   client.begin(IOT_ENDPOINT, net);
-  Serial.print("Connecting to MQTT: %s ", IOT_ENDPOINT);
-  while (!client.connect(THINGNAME, IOT_USER, IOT_PASSWORD)) {
+  Serial.print("Connecting to MQTT ..");
+  while (!client.connect(THINGNAME)) {
     Serial.print(".");
+
+    strip.clear();
+    led_color = strip.Color(255, 251, 0); // Yellow
+    for (int i = 0; i < LED_COUNT; i++) {
+      strip.setPixelColor(i, led_color);
+      strip.show();
+    }
     delay(100);
   }
-
-  if (!client.connected()) {
-    Serial.println("MQTT Timeout!");
-    return;
-  }
+  
+  strip.clear();
+  strip.show();
 
   client.onMessage(onMqttMessage);
   // Subscribe to a topic
   client.subscribe(IOT_SUBSCRIBE_TOPIC);
 
   Serial.println("MQTT Connected!");
+  Serial.print("Broker IP: ");
+  Serial.println(IOT_ENDPOINT);
 }
-
 
 void initLed()
 {
@@ -57,29 +73,29 @@ void initLed()
 
 void setup() {
   Serial.begin(115200);
+  
   initLed();
+
+  // WIFI Connection
   initWiFi();
-  Serial.print("RRSI: ");
-  Serial.println(WiFi.RSSI());
+
+  // IOT Connection
   initIOT();
 }
-
-
 
 void loop() {
   client.loop();
 
-  // if WiFi is down, try reconnecting every CHECK_WIFI_TIME seconds
-  unsigned long currentMillis = millis();
-  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval)) {
-    Serial.print(millis());
-    Serial.println("Reconnecting to WiFi...");
-    WiFi.disconnect();
-    WiFi.reconnect();
-    previousMillis = currentMillis;
+   // if WiFi is down, try reconnecting
+  if (WiFi.status() != WL_CONNECTED){
+    initWiFi();
+  }
+
+  // if MQQT is down, try reconnecting
+  if (!client.connected()){
+    initIOT();
   }
 }
-
 
 void onMqttMessage(String &topic, String &payload) {
   StaticJsonDocument<200> doc;
@@ -91,26 +107,25 @@ void onMqttMessage(String &topic, String &payload) {
 }
 
 void stateChange(int state) {
+  //On-Off Air LED Trigger
 
   char jsonBuffer[512];
 
   if (state == 1) {
-    Serial.println("LED ON");
+    Serial.println("ON AIR TRIGGER");
     strip.clear();
-    led_color = strip.Color(250, 255, 255);
+    led_color = strip.Color(255, 0, 0);
     for (int i = 0; i < LED_COUNT; i++) {
-      Serial.print("O");
       strip.setPixelColor(i, led_color);
       strip.show();
     }
   }
   else {
     state = 0;
-    Serial.println("LED OFF");
+    Serial.println("OFF AIR TRIGGER");
     led_color = strip.Color(0, 0, 0);
     strip.clear();
     for (int i = 0; i < LED_COUNT; i++) {
-      Serial.print("X");
       strip.setPixelColor(i, led_color);
       strip.show();
     }
